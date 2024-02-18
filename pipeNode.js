0e6926536c5639fb30faba81542fa4bfa33f8a02
Elkwizard <bananamanthefourth@gmail.com>
2024-02-18 04:34:34 -0800

    added a command line REPL

--- pipeNode.js
+++ pipeNode.js
@@ -20,7 +20,7 @@ const document = {
 };
 
 const {
-	scopes, currentScope, evalStat, Operator, Type,
+	scopes, currentScope, evalStat, Operator, Type, List,
 	NUMBER_REGEX, SINGLE_LINE_COMMENT_REGEX,
 	MULTILINE_COMMENT_REGEX
 } = run(
@@ -41,64 +41,11 @@ Array.prototype.join = function (sep) {
 	return result;
 };
 
-const escape = x => `\x1b[${x}m`;
-const color = (name, text) => {
-	const colorMap = {
-		"black": 30,
-		"red": 31,
-		"green": 32,
-		"yellow": 33,
-		"blue": 34,
-		"magenta": 35,
-		"cyan": 36,
-		"light gray": 37,
-		"dark gray": 90,
-		"light red": 91,
-		"light green": 92,
-		"light yellow": 93,
-		"light blue": 94,
-		"light magenta": 95,
-		"light cyan": 96,
-		"white": 97
-	};
-	if (!(name in colorMap)) return name;
-	return escape(colorMap[name]) + text + escape(39);
-};
-
-const underline = text => escape(4) + text + escape(24);
-
-const background = (name, text) => {
-	const colorMap = {
-		"black": 40,
-		"red": 41,
-		"green": 42,
-		"yellow": 43,
-		"blue": 44,
-		"magenta": 45,
-		"cyan": 46,
-		"light gray": 47,
-		"dark gray": 100,
-		"light red": 101,
-		"light green": 102,
-		"light yellow": 103,
-		"light blue": 104,
-		"light magenta": 105,
-		"light cyan": 106,
-		"white": 107
-	};
-	if (!(name in colorMap)) return name;
-	return escape(colorMap[name]) + text + escape(49);
-};
-
-const deformat = text => text.replace(/\x1b\[(\d+?)m/g, "");
-
-module.exports = { color, underline, deformat, background };
+const { color } = require("./formatting");
 
 function log(message, col = "white") {
 	console.log(color(col, message));
 }
-
-
 
 function logHTML(message, prefix = "") {
 	const usePalette = (name, palette) => {
@@ -200,7 +147,4 @@ currentScope["typeof"] = new Operator([
 
 currentScope["clear"] = new Operator([], () => clear());
 
-const [_node, _this, file] = process.argv;
-const content = fs.readFileSync(file, "utf-8");
-
-exec(content);
+module.exports = { exec, currentScope, Operator, Type, List };
--- repl.js
+++ repl.js
@@ -1,4 +1,17 @@
-const fs = require("fs");
+const { exec, currentScope, Operator, Type, List } = require("./pipeNode");
 
-eval(fs.readFileSync("pipelang.js", "utf-8"));
+currentScope["exit"] = new Operator([], () => process.exit(0));
 
+function prompt() {
+	process.stdout.write("> ");
+}
+
+process.stdin.on("data", data => {
+	data = data.toString("utf-8").replace(/[\r\n]/g, " ");
+	process.stdout.write("\x1b[1A");
+	prompt();
+	exec(data);
+	prompt();
+});
+
+prompt();
